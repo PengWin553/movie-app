5078485d37fb23bcb8c3e75b3551ca5e0f6e0793
PengWin553 <victoriakobayashi553@gmail.com>
2025-06-11 21:16:13 +0800

    Logical AND (&&) Conditional Rendering
    A cleaner and more logical alternative to Ternary Conditional Rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,21 @@
 // Importing the CSS file for global styling
 import './App.css'
-
 // Importing the MovieCard component from the components directory
 import MovieCard from './components/MovieCard'
 
 // Main App component
 function App() {
 
-  const movieNumber = 1;
+  const movieNumber = 2;
 
   return (
     <>
-      {movieNumber === 1 ?
-        (
-          <MovieCard movie={{ title: "Cat's Movie", release_date: "2024", url: 'https://i.pinimg.com/originals/0e/0a/f3/0e0af37e3cc1d48ef19bd51973c49719.jpg' }} />
-        ) 
-      :
-        (
-          <MovieCard movie={{ title: "Mae's Movie", release_date: "2025", url: 'https://vignette.wikia.nocookie.net/maid-dragon/images/5/52/Kobayashi_5.png/revision/latest?cb=20170315190544' }} />
-        )
+      {movieNumber === 1 && 
+        <MovieCard movie={{ title: "Cat's Movie", release_date: "2024", url: 'https://i.pinimg.com/originals/0e/0a/f3/0e0af37e3cc1d48ef19bd51973c49719.jpg' }} />
+      }
+
+      {movieNumber === 2 && 
+        <MovieCard movie={{ title: "Mae's Movie", release_date: "2025", url: 'https://vignette.wikia.nocookie.net/maid-dragon/images/5/52/Kobayashi_5.png/revision/latest?cb=20170315190544' }} />
       }
     </>
   )
